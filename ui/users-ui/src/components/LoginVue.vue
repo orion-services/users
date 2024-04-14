f9944e73a2530291e8aadb7df044369ba2b14422
Rodrigo Prestes Machado <rodrigo.prestes@gmail.com>
2024-04-14 18:48:14 -0300

    sessionStorage

--- ui/users-ui/src/components/LoginVue.vue
+++ ui/users-ui/src/components/LoginVue.vue
@@ -1,20 +1,22 @@
 <template>
-  <div class="columns">
-    <div class="column"></div>
-    <div class="column">
-      <div class="card">
-        <div class="card-content">
-          <div class="content">
-            <h1 class="title is-4">Users</h1>
-            <input class="input" type="text" placeholder="Name" v-model="name" />
-            <input class="input" type="text" placeholder="E-mail" v-model="email" />
-            <input class="input" type="password" placeholder="Password" v-model="password" />
-            <button class="button" @click="login()">login</button>
+  <div class="container">
+    <div class="columns is-centered">
+      <div class="column is-half">
+        <div class="card">
+          <div class="card-content mt-6">
+            <div class="content">
+              <figure class="image is-64x64 mb-5">
+                <img class="is-rounded" src="../../assets/logo.png" alt="Logo" />
+              </figure>
+              <input class="input mb-3" type="text" placeholder="Name" v-model="name" />
+              <input class="input mb-3" type="text" placeholder="E-mail" v-model="email" />
+              <input class="input mb-3" type="password" placeholder="Password" v-model="password" />
+              <button class="button" @click="createUser()">Create user</button>
+            </div>
           </div>
         </div>
       </div>
     </div>
-    <div class="column"></div>
   </div>
 </template>
 
@@ -24,25 +26,26 @@ import axios from 'axios'
 export default {
   data() {
     return {
+      api: 'http://localhost:8080/api/users/createAuthenticate',
       name: '',
       email: '',
-      password: ''
+      password: '',
+      authenticated: false
     }
   },
   methods: {
-    login() {
-        const params = new URLSearchParams()
-        params.append('name', this.name)
-        params.append('email', this.email)
-        params.append('password', this.password)
+    async createUser() {
+      const params = new URLSearchParams()
+      params.append('name', this.name)
+      params.append('email', this.email)
+      params.append('password', this.password)
 
-        axios.post('http://localhost:8080/api/users/createAuthenticate', params)
-        .then((response) => {
-          console.log(response.data)
-        })
-        .catch((error) => {
-          console.log(error)
-        })
+      try {
+        let response = await axios.post(this.api, params)
+        sessionStorage.setItem('users', JSON.stringify(response.data))
+      } catch (error) {
+        console.log(error)
+      }
     }
   }
 }
